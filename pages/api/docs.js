83de2d52fbc6576800f8ac3e5a4409d72170d1a5
Tomasz Kowalski <88949668+tomek-kowalski@users.noreply.github.com>
2025-03-12 15:56:12 +0100

    api changed

--- pages/api/docs.js
+++ pages/api/docs.js
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "./auth/[...nextauth]";
+import { authOptions } from "./docs.js";
 import fs from "fs";
 import path from "path";
 
