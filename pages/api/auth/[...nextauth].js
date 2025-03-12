07d526d55204a2b4f872f5f0d7c404bbc5525cc9
Tomasz Kowalski <88949668+tomek-kowalski@users.noreply.github.com>
2025-03-12 18:24:37 +0100

    google auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-const allowedUsers = ["alloweduser1@gmail.com", "alloweduser2@example.com", "kc.tomasz.kowalski@gmail.com"];
-
 export const authOptions = {
   providers: [
     GoogleProvider({
@@ -10,17 +8,11 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  callbacks: {
-    async signIn({ user }) {
-      if (allowedUsers.includes(user.email)) {
-        return true; // ✅ Allow access
-      }
-      return false; // ❌ Deny access
-    },
-    async session({ session }) {
-      return session;
-    },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    error: "/auth/error",
   },
+  debug: true,
 };
 
 export default NextAuth(authOptions);
