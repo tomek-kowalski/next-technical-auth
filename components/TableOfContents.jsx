a00c27678ad6ee183be22a1c66f5d8816a26cdcc
Tomasz Kowalski <88949668+tomek-kowalski@users.noreply.github.com>
2025-03-13 15:21:30 +0100

    tuther imporovments

--- components/TableOfContents.jsx
+++ components/TableOfContents.jsx
@@ -2,37 +2,49 @@ import { useEffect } from "react";
 
 export default function TableOfContents() {
   useEffect(() => {
-    const tableOfContents = Array.from(document.querySelectorAll("table th"))
-      .find(th => th.innerText.trim() === "Table of Contents");
+    console.log('it is loaded' );
+    const handleClick = (e) => {
+      e.preventDefault();
 
-    if (tableOfContents) {
-      const tocLinks = tableOfContents.closest("table").querySelectorAll("td a[href^='#']");
-
-      tocLinks.forEach(link => {
-        link.addEventListener("click", handleClick);
-      });
-
-      return () => {
-        tocLinks.forEach(link => {
-          link.removeEventListener("click", handleClick);
+      const targetId = e.target.getAttribute("href").substring(1);
+      console.log(targetId );
+      const targetElement = document.getElementById(targetId);
+      
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
         });
-      };
-    }
+      }
+    };
+
+    const addClickListeners = () => {
+      const tableOfContents = Array.from(document.querySelectorAll("table th"))
+        .find(th => th.innerText.trim() === "Table of Contents");
+
+      if (tableOfContents) {
+        const tocLinks = tableOfContents.closest("table").querySelectorAll("td a[href^='#']");
+        
+        tocLinks.forEach(link => {
+          link.addEventListener("click", handleClick);
+        });
+        
+        return () => {
+          tocLinks.forEach(link => {
+            link.removeEventListener("click", handleClick);
+          });
+        };
+      }
+    };
+
+ 
+    const cleanup = addClickListeners();
+
+
+    return () => {
+      if (cleanup) cleanup();
+    };
   }, []);
-
-  const handleClick = (e) => {
-    e.preventDefault();
-
-    const targetId = e.target.getAttribute("href").substring(1);
-
-    const targetElement = document.getElementById(targetId);
-    if (targetElement) {
-      targetElement.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
-    }
-  };
 
   return null;
 }
