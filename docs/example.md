97a5b80ddb9a44259fda8cd8ffec22786b553ab2
Tomasz Kowalski <88949668+tomek-kowalski@users.noreply.github.com>
2025-03-13 13:38:02 +0100

    table nchor

--- docs/example.md
+++ docs/example.md
@@ -1,4 +1,4 @@
-# [tuneupfitness.com](https://www.tuneupfitness.com) - Optimization plan 
+## tuneupfitness.com - Optimization plan
 
 | Prepared by | Version | Date |
 | --- | ---| ---|
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+});
+
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
 };
 
-export default nextConfig;
+module.exports = withMDX(nextConfig);
